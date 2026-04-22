9ddfd9c7e1cbf8955e10f0c71cd68cc3862da247
Adarsh Dadwal <adarshdadwal25@gmail.com>
2026-04-22 11:25:57 +0530

    Revert "Initial commit"
    This reverts commit 68cc0ce614601651c013bc42a1ebc742e9363ac2.
    
    :wq

--- launch_gui.py
+++
@@ -1,7 +0,0 @@
-import webview
-
-frontend_url = 'http://localhost:5173/'  # or your built site location
-
-webview.create_window("Resume Screener", frontend_url, width=1200, height=800)
-webview.start()
-
