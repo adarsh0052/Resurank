edeceae2003f87bb11f0b2ab920633cdc0fdbdc3
Adarsh Dadwal <133523790+adarsh0052@users.noreply.github.com>
2026-04-28 15:04:07 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        {/* Left: Logo and brand name */}
+        <div className="logo-section">
+          <img src="src\assets\LOGO.png" alt="logo" className="logo-img" style={{ height: '70px', width: '70px' }} />
+          <span className="brand-name" style={{ marginLeft: '30px'}}>RESURANK</span>
+        
+        </div>
+
+    
+        <nav className="nav-links">
+          <div className="nav-item dropdown" > 
+            <a href="/" style={{ color: '#f9fafb',textDecoration:'none' }}>Home</a>
+           
+          </div>
+          {/* <div className="nav-item dropdown">
+          <a href="/pricing"style={{ color: '#f9fafb', textDecoration: 'none' }} >Pricing</a>
+            
+          </div> */}
+          <div className="nav-item dropdown">
+          <a href="/tools" style={{ color: '#f9fafb', textDecoration: 'none' }}>Tools</a>
+            
+          </div>
+          <div className="nav-item dropdown">
+            <a href="/about" style={{ color: '#f9fafb', textDecoration: 'none' }}>About Us</a>
+           
+          </div>
+          <div className="nav-item dropdown">
+            <a href="/help"style={{ color: '#f9fafb', textDecoration: 'none' }}>Help</a>
+          
+          </div>
+        </nav>
+
+        {/* Right: Auth buttons */}
+        <div className="auth-buttons">
+        <Link to="/login">
+          <button className="sign-in">Log In</button>
+        </Link> 
+       <Link to="/signup">  
+          <button className="sign-in">Sign Up</button>
+        </Link> 
+          
+        </div>
+      </div>
+       
+    </header>
+  );
+}
+
+export default Header;
